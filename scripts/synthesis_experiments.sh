c34631fc1c1d837c7e21cbeefd62f7db3527e380
Javier Segovia <jsego6@gmail.com>
2021-07-21 12:12:02 +0200

    Update synthesis_experiments.sh

--- scripts/synthesis_experiments.sh
+++ scripts/synthesis_experiments.sh
@@ -1,6 +1,6 @@
 domains=( "corridor" "fibo" "find" "gripper" "reverse" "select" "sorting" "triangular-sum" "visitall" )
 bounds=(8 7 6 8 7 7 11 6 8)
-heuristics=("h1" "h2" "h3" "h4" "h5" "h6" "h7" "h8" "h9" "h5 h1" )
+heuristics=("h1" "h2" "h3" "h4" "h5" "h6" "h7" "h8" "h9" )
 
 total=${#domains[@]}
 #total=1
